c35269334c258a7b6e3c44b66a13d398fc084a90
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-09-10 14:23:53 +0300

    main.py

--- checkers game/main.py
+++ checkers game/main.py
@@ -2,19 +2,16 @@ import pygame
 import sys
 import random
 
-# Renk tanımları
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-# Ekran boyutları
 WIDTH, HEIGHT = 500, 500
 ROWS, COLS = 8, 8
 SQUARE_SIZE = WIDTH // COLS
 
-# Taşlar için resimler
 CROWN = pygame.transform.scale(pygame.image.load('crown.png'), (44, 25))
 
 
