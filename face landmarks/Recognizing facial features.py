ae85a48a36f642f281bdb76f6c3d980d2a493850
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-11-12 15:30:39 +0300

    Update Recognizing facial features.py

--- face landmarks/Recognizing facial features.py
+++ face landmarks/Recognizing facial features.py
@@ -4,15 +4,12 @@ import mediapipe as mp
 mp_face_detection = mp.solutions.face_detection
 mp_face_mesh = mp.solutions.face_mesh
 mp_drawing = mp.solutions.drawing_utils
-
 cap = cv2.VideoCapture(0)
 
 with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5) as face_mesh:
-
     while True:
         _, frame = cap.read()
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        
         results = face_mesh.process(frame_rgb)
 
         if results.multi_face_landmarks:
@@ -24,7 +21,6 @@ with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence
                     cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
         
         cv2.imshow("Frame", frame)
-
         key = cv2.waitKey(1)
         if key == 27:  # ESC tuşuna basınca çık
             break
