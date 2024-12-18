37298d389d6043e9b916437b8aded8fa918baf80
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-12-18 15:35:58 +0300

    Update main.py

--- WhatsAppMSSender/main.py
+++ WhatsAppMSSender/main.py
@@ -5,25 +5,22 @@ import datetime
 
 def send_message_via_whatsapp(phone_number, message, delay=10):
     """
-    :param phone_number: Phone number to send (ör. '+905xxxxxxxxx')
+    :param phone_number: Phone number to send ('+905xxxxxxxxx')
     :param message: Text of message to be sent
     :param delay: Waiting time after opening WhatsApp (default 10 seconds)
     """
     os.system("start whatsapp://send?phone=" + phone_number)
-    time.sleep(delay)  # Uygulamanın açılması ve QR kodun taranması için bekle
+    time.sleep(delay) 
 
-    # Mesajı yaz ve gönder
     pyautogui.write(message)
     pyautogui.press("enter")
 
-# Kullanıcıdan veri al
 phone_number = input("Alıcı telefon numarasını girin (+90 ile başlayarak): ")
 message = input("Gönderilecek mesajı yazın: ")
 
 send_time = input("Mesajın gönderileceği zamanı girin (HH:MM formatında): ")
 send_hour, send_minute = map(int, send_time.split(":"))
 
-# Şu anki zamanı kontrol ederek bekle
 now = datetime.datetime.now()
 send_at = now.replace(hour=send_hour, minute=send_minute, second=0, microsecond=0)
 
