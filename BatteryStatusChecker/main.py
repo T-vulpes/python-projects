8d5926ba69699a045366cccc6329f02504692516
Deniz <167799743+T-vulpes@users.noreply.github.com>
2025-03-29 14:52:20 +0300

    Update main.py

--- BatteryStatusChecker/main.py
+++ BatteryStatusChecker/main.py
@@ -5,12 +5,10 @@ def format_time(seconds_left):
     hours, minutes = divmod(minutes, 60)  
     return "%d:%02d:%02d" % (hours, minutes, seconds)
 
-# Pil durumu bilgisini alıyoruz
 battery_info = psutil.sensors_battery()
 battery_percentage = battery_info.percent  
 remaining_time = format_time(battery_info.secsleft) if battery_info.secsleft != -1 else "Calculating..."  
 
-# Adaptör takılı mı?
 power_plugged_in = battery_info.power_plugged  
 
 # Sonuçları ekrana yazdırıyoruz
