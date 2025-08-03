711e0195266cddb10a40663abb68d9265d93bd31
Deniz <167799743+T-vulpes@users.noreply.github.com>
2025-08-03 16:16:24 +0300

    TicTacToe game beginner

--- bsc_tictactoe.py
+++ bsc_tictactoe.py
@@ -50,13 +50,11 @@ class TicTacToe:
 
     def check_winner(self):
         b = self.board
-        # Satırlar ve sütunlar
         for i in range(3):
             if b[i][0] == b[i][1] == b[i][2] != "":
                 return b[i][0]
             if b[0][i] == b[1][i] == b[2][i] != "":
                 return b[0][i]
-        # Çaprazlar
         if b[0][0] == b[1][1] == b[2][2] != "":
             return b[0][0]
         if b[0][2] == b[1][1] == b[2][0] != "":
@@ -77,4 +75,5 @@ if __name__ == "__main__":
     root = tk.Tk()
     game = TicTacToe(root)
     root.mainloop()
+
 
